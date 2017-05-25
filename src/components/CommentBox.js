b630db560ee8439f60b95beb871bd0a060ce374d
codeedoc <710987071@qq.com>
2017-05-25 18:58:38 +0800

    i

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,16 +1,24 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import store from '../redux/store';
+
 
 class CommentBox extends React.Component {
   constructor(){
     super();
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.state={
-      comments:[
-        'hello1',
-        'hello2',
-      ]
+      comments:store.getState()
     }
   }
+  handleSubmit(e){
+    e.preventDefault()
+    const content = this.refs.content.value;
+    this.setState({ comments: [...this.state.comments, content] })
+    this.refs.commentForm.reset()
+
+  }
   render () {
+
     let commentList = this.state.comments.map((item,i) => {
       return (<div key={i} className='comment'>
         {item}
@@ -19,8 +27,8 @@ class CommentBox extends React.Component {
     return(
       <div className="comment-box">
         {commentList}
-        <form className='comment-form'>
-          <input type='text' className='input' />
+        <form ref='commentForm' onSubmit={this.handleSubmit} className='comment-form'>
+          <input type='text' className='input' ref='content'/>
           <button type='submit' className='submit-btn'>提交</button>
 
         </form>
